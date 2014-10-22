ac53e3df528df5c3ed95779bb20f50854c985bc0
Andrew Kane <acekane1@gmail.com>
2014-10-21 23:49:04 -0700

    Use timeout as documentation states

--- lib/slowpoke/railtie.rb
+++ lib/slowpoke/railtie.rb
@@ -2,7 +2,7 @@ module Slowpoke
   class Railtie < Rails::Railtie
 
     initializer "slowpoke" do
-      Rack::Timeout.service_timeout = Slowpoke.timeout
+      Rack::Timeout.timeout = Slowpoke.timeout
       ActiveRecord::Base.logger.class.send(:include, ::LoggerSilence) if ActiveRecord::Base.logger
     end
 
