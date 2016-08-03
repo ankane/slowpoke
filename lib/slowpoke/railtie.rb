17415988a8b6fcdccc15e19a01b4154b4f5c7b47
J-P Teti <jp_teti@icloud.com>
2016-08-02 22:06:19 -0400

    Changed railtie to use class references since passing strings is deprecated in Rails 5. (#10)

--- lib/slowpoke/railtie.rb
+++ lib/slowpoke/railtie.rb
@@ -3,7 +3,7 @@ module Slowpoke
     initializer "slowpoke" do |app|
       Rack::Timeout.timeout = Slowpoke.timeout
 
-      app.config.middleware.insert_before "ActionDispatch::RemoteIp", "Rack::Timeout"
+      app.config.middleware.insert_before ActionDispatch::RemoteIp, Rack::Timeout
 
       # prevent RequestExpiryError from killing web server
       app.config.middleware.insert 0, Slowpoke::Middleware
