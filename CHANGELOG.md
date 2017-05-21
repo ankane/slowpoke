6a04e5da25bfba9b94719682c78010cac601bcad
Andrew Kane <acekane1@gmail.com>
2017-05-21 13:17:27 -0700

    Fixed custom error pages for Rails 5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
-## 0.1.4 [unreleased]
+## 0.2.0 [unreleased]
 
+- Fixed custom error pages for Rails 5.1
 - Fixed migration statement timeout
 
 ## 0.1.3
--- lib/slowpoke/railtie.rb
+++ lib/slowpoke/railtie.rb
@@ -2,7 +2,11 @@ module Slowpoke
   class Railtie < Rails::Railtie
     initializer "slowpoke" do |app|
       Rack::Timeout.timeout = Slowpoke.timeout
-      app.config.middleware.insert_before ActionDispatch::RemoteIp, Rack::Timeout
+      if Rails::VERSION::MAJOR >= 5
+        app.config.middleware.insert_after ActionDispatch::DebugExceptions, Rack::Timeout
+      else
+        app.config.middleware.insert_before ActionDispatch::RemoteIp, Rack::Timeout
+      end
       app.config.middleware.insert 0, Slowpoke::Middleware
     end
   end
