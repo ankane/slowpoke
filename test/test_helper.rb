c8bc938f8c5e04d8c91e55796715d706ee1c4a6b
Andrew Kane <andrew@ankane.org>
2025-04-21 20:59:17 -0700

    Updated tests for JRuby [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,5 +22,5 @@ end
 
 # https://github.com/rails/rails/issues/54595
 if RUBY_ENGINE == "jruby" && Rails::VERSION::MAJOR >= 8
-  Rails.application.routes_reloader.execute
+  Rails.application.reload_routes_unless_loaded
 end
