13bcbaf1460b8af3a84b604cf3a9056936667ce4
Andrew Kane <andrew@ankane.org>
2025-05-30 00:35:22 -0700

    Improved code [skip ci]

--- lib/generators/slowpoke/install_generator.rb
+++ lib/generators/slowpoke/install_generator.rb
@@ -3,7 +3,7 @@ require "rails/generators"
 module Slowpoke
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("templates", __dir__)
 
       def copy_503_html
         template "503.html", "public/503.html"
